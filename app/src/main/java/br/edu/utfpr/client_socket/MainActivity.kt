f1c3b79acf8aed74a7bf430a9d5768c07038b1d0
Robison Cris Brito <44240574+robisoncrisbrito@users.noreply.github.com>
2026-04-11 14:24:41 -0300

    Aplicativo com o AsyncTask, porém, sem incremento no progressbar

--- app/src/main/java/br/edu/utfpr/client_socket/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/client_socket/MainActivity.kt
@@ -78,23 +78,55 @@ class MainActivity : AppCompatActivity() {
                     val ip = BuildConfig.SERVER_IP
                     val port = BuildConfig.SERVER_PORT
 
+                    publishProgress(1)
+                    Thread.sleep(1000)
+
                     clientSocket = Socket(ip, port) //linha é bloqueante ou dará exceção
                     //Conectado com o Server
 
+                    publishProgress(2)
+                    Thread.sleep(1000)
+
                     outputStream =
                         clientSocket.getOutputStream().bufferedWriter(Charset.forName("utf-8"))
+
+                    publishProgress(3)
+                    Thread.sleep(1000)
+
                     inputStream =
                         clientSocket.getInputStream().bufferedReader(Charset.forName("utf-8"))
                     //Fluxo de IO Criado
+
+                    publishProgress(4)
+                    Thread.sleep(1000)
                 }
 
 
                 outputStream.write( protocol[0] + "\n")
+
+                publishProgress(5)
+                Thread.sleep(1000)
+
                 outputStream.flush()
                 //Mensagem enviada ao servidor, sem bloqueios
 
+                publishProgress(6)
+                Thread.sleep(1000)
+
                 val result = inputStream.readLine() //linha bloqueante
                 //mensagem recebida do servidor
+
+                publishProgress(7)
+                Thread.sleep(1000)
+
+                publishProgress(8)
+                Thread.sleep(1000)
+
+                publishProgress(9)
+                Thread.sleep(1000)
+
+                publishProgress(10)
+                Thread.sleep(1000)
 
                 return result
 
@@ -108,6 +140,10 @@ class MainActivity : AppCompatActivity() {
             tvResultado.text = result
             progressBar.visibility = View.GONE
             btEnviar.isEnabled = true
+        }
+
+        override fun onProgressUpdate(vararg progresso: Int?) {
+            progressBar.setProgress( progresso[0] ?: 0)
         }
 
     }
